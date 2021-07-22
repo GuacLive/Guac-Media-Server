illuspas/Node-Media-Server

Close correct session when no players are found in "pull" mode.

src/node_relay_server.js
@@ -147,7 +147,7 @@ class NodeRelayServer {
   }
 
   onDonePlay(id, streamPath, args) {
-    let session = this.dynamicSessions.get(id);
+    let session = Array.from(this.dynamicSessions, ([name, value]) => value ).find((session)=>{return session.conf.name === streamPath.split('/')[2]});
     let publisher = context.sessions.get(context.publishers.get(streamPath));
     if (session && publisher.players.size == 0) {
       session.end();

GuacLive/Guac-Media-Server

src/node_relay_server.js
@@ -187,7 +187,7 @@ class NodeRelayServer {
   }
 
   onDonePlay(id, streamPath, args) {
-    let list = this.dynamicSessions.get(id);
+    let list = Array.from(this.dynamicSessions, ([name, value]) => value ).find((session)=>{return session.conf.name === streamPath.split('/')[2]});
     let publisher = context.sessions.get(context.publishers.get(streamPath));
     if (list && publisher.players.size == 0) {
       list.slice().forEach(session => session.end());